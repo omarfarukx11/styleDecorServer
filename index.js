52782fd3805b6fdde9b176f02017e29d1ec7d8e8
Omar Faruk <khfarukcht@mail.com>
2025-12-06 00:44:27 +0600

    mongodb connect success

--- index.js
+++ index.js
@@ -3,20 +3,82 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 3000;
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.oyyjehq.mongodb.net/?appName=Cluster0";
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
+// mongoDB Connection 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oyyjehq.mongodb.net/?appName=Cluster0`;
 
 // middleWare
 app.use(express.json());
 app.use(cors());
 
-
-
 app.get("/", (req, res) => {
   res.send("lets begin");
 });
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    await client.connect();
+    
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// --------------------------------------------
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
